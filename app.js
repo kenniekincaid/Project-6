61e187a346bd4180ff9fccd408b9d19f5dd4a436
Kendra Roberson <kendra.roberson@verizon.com>
2019-08-14 01:08:21 -0500

    End of day commit

--- app.js
+++ app.js
@@ -1,22 +1,58 @@
-const express = require('express');
-const bodyParser = require('body-parser');
 
+//Set up Express server
+const express = require('express');//Set up Express server
+const projects = require('./data.json');
+const bodyParser = require('body-parser');
+const router = express.Router();
+//Express app created
 const app = express();
-const port = 3000;
+
+//Middleware to use static files like CSS...
+app.use('/static', express.static('public'));
+
+//Middleware: Allows me to use Pug to render templates/views.
+app.set('view engine', 'pug');
 
 app.use(bodyParser.urlencoded({ extended: false}));
 
-app.set('view engine', 'pug');
+//Express will look for files with the Pug extension...
+app.get('/', (req, res) => {//express knows to look for pug files b/c set to view engine, pug...
+    res.locals.data.projects;
+    res.render('index', `${projects}`);
+});
 
-const routes = require('.routes');
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/project', (req, res) => {
+    res.locals.data.projects;
+    res.render('project', `${projects}`);
+});
+
+// function errorMessage() {
+//     app.use((req, res, next) => {
+//         if (error === 404) {
+//             app.get('/error', (req, res) => {
+//                 res.render('error');
+//                 console.log('404 - Page Not Found!');
+//             });
+//         } else if (error === 500) {
+//             app.get('/error', (req, res) => {
+//                 res.render('error');
+//                 console.log('500 - Page does not exist');
+//             });
+//         }
+//     });
+// };
+
+//set up routes
+const routes = require('./routes');
 
 app.use(routes);
 
-app.use((req, res, next) => {
-    console.log("Hello");
-    const err = new Error('404 Page Not Found');
-    err.status = 404;
-    next(err);
+app.get('/', (req, res) => {
+    res.send('<h1>Welcome! This is Kennie Kincaid!</h1>');//pass in template
 });
 
 app.use((req, res, next) => {
@@ -28,10 +64,16 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
     res.locals.error = err;
     res.status(err.status);
-    res.render('error');
+    res.render('error', {error});
 });
 
-app.listen(port, () => console.log(`App is listening on port ${port}!`))
+const port = 3000;
+app.listen(port, () => console.log(`App is listening on port ${port}!`));
 
+module.exports = router
 
+// Server to repond to requests
 
+// const routes = require('.routes');
+
+// app.use(routes);
