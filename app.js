f9d4bd067b944720cdc50565faeed40ab5ffb997
Kendra Roberson <kendra.roberson@verizon.com>
2019-08-14 17:05:43 -0500

    Almost ready to submit. Codes working!

--- app.js
+++ app.js
@@ -1,80 +1,39 @@
+/**REQUIRED: SERVER, DATA, PARSER, & APP */
+    const express = require('express');
+    const { projects } = require('./data.json');
+    const bodyParser = require('body-parser');
+    const app = express();
 
-//Set up Express server
-const express = require('express');//Set up Express server
-const { projects } = require('./data.json');
-const bodyParser = require('body-parser');
-// const router = express.Router();
-//Express app created
-const app = express();
+/**MIDDLEWARE SECTION: Static files and pug to render templates/views*/
+    app.use('/static', express.static('public'));
+    app.set('view engine', 'pug');
+    app.use(bodyParser.urlencoded({ extended: false }));
 
-//Middleware to use static files like CSS...
-app.use('/static', express.static('public'));
+/**ROUTES: Express will look for files with the pug ext b/c set to view engine.*/
+    app.get('/', (req, res) => {
+        res.locals.projects = projects;
+        res.render('index');
+    });
+    app.get('/about', (req, res) => {
+        res.render('about');
+    });
+    app.get('/project/:id', (req, res) => {
+        res.locals.project = projects[req.params.id - 1];
+        res.render('project');
+    });
 
-//Middleware: Allows me to use Pug to render templates/views.
-app.set('view engine', 'pug');
+/**ERROR HANDLERS...*/
+    app.use((req, res, next) => {
+        const err = new Error('Oops! Page Not Found');
+        err.status = 404;
+        next(err);
+    });
+    app.use((err, req, res, next) => {
+        err.status = err.status || 500;
+        res.status(err.status);
+        res.render('error', {error: err});
+    });
 
-app.use(bodyParser.urlencoded({ extended: false}));
-
-//Express will look for files with the Pug extension...
-app.get('/', (req, res) => {//express knows to look for pug files b/c set to view engine, pug...
-    res.locals.projects = projects;
-    res.render('index');
-});
-
-app.get('/about', (req, res) => {
-    res.render('about');
-});
-
-app.get('/project', (req, res) => {
-    res.locals.data.projects;
-    res.render('project', `${projects}`);
-});
-
-// function errorMessage() {
-//     app.use((req, res, next) => {
-//         if (error === 404) {
-//             app.get('/error', (req, res) => {
-//                 res.render('error');
-//                 console.log('404 - Page Not Found!');
-//             });
-//         } else if (error === 500) {
-//             app.get('/error', (req, res) => {
-//                 res.render('error');
-//                 console.log('500 - Page does not exist');
-//             });
-//         }
-//     });
-// };
-
-//set up routes
-// const routes = require('./routes');
-
-// app.use(routes);
-
-// app.get('/', (req, res) => {
-//     res.send('<h1>Welcome! This is Kennie Kincaid!</h1>');//pass in template
-// });
-
-app.use((req, res, next) => {
-    const err = new Error('Oops! Page Not Found');
-    err.status = 404;
-    next(err);
-});
-
-app.use((err, req, res, next) => {
-    // res.locals.error = err;
-    err.status = err.status || 500;
-    res.status(err.status);
-    res.render('error', {error: err});
-});
-
-const port = 3000;
-app.listen(port, () => console.log(`Kennie's fabulous app is listening on port ${port}!`));
-
-// module.exports = router
-
-// Server to repond to requests
-
-// const routes = require('.routes');
-
-// app.use(routes);
+/**PPORT LISTE ERT...*/
+    const port = 3000;
+    app.listen(port, () => console.log(`Kennie's fabulous app is listening on port ${port}!`));
